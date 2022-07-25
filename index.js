3d22c5087d3021611c7f72d8175017aa3bedfc54
Danny <lannyducas@gmail.com>
2022-07-25 15:17:26 -0400

    commit tree cdd6015b839bdf4e251664761c747aa380685045

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -9,6 +11,7 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    console.log(books);
   });
 }
 
